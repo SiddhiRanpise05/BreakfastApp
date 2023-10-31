d6e55b3f583e5efae50e2de0867f6b31cce32745
SiddhiRanpise05 <siddhiranpise80@gmail.com>
2023-10-31 21:57:38 +0530

    My first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:business/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: HomePage(
+        key:
+            UniqueKey(), // You can use UniqueKey() or another Key instance here
+      ),
+    );
+  }
+}
